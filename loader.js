3b9914fea3efb41d2ccdf0bbf9c54fd24f92d0cc
gameland125 <farzad03x@gmail.com>
2026-07-07 18:42:11 +0330

    Delete loader.js

--- loader.js
+++
@@ -1,9 +0,0 @@
-function loadPayload(path){
-    fetch(path)
-    .then(res => res.arrayBuffer())
-    .then(buf => {
-        console.log("Payload Loaded");
-        // اینجا inject واقعی انجام میشه
-    })
-    .catch(() => alert("Payload load error"));
-}
