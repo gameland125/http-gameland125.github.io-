eefdc29ddd480b9070213748f9c96289a048cdbe
gameland125 <farzad03x@gmail.com>
2026-07-07 22:59:46 +0330

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,4 +1,14 @@
-// loader.js
-function loadPayload(path) {
-   // fetch + inject
+function loadPayload(path){
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', path, true);
+    xhr.responseType = "arraybuffer";
+
+    xhr.onload = function(){
+        if(this.status === 200){
+            var payload = new Uint8Array(this.response);
+            sendPayload(payload);
+        }
+    };
+
+    xhr.send();
 }
