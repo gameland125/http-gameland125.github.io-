da73a1f8a5e7eda2fd300f2e242e57706db76098
gameland125 <farzad03x@gmail.com>
2026-07-08 09:00:43 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,13 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('ps4-cache').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/auto.html',
-        '/offline.html',
-        '/payloads/goldhen.bin',
-        '/exploit/exploit.js',
-        '/exploit/loader.js',
-        '/exploit/rop.js',
-        '/exploit/syscalls.js'
-      ]);
-    })
-  );
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
-  );
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('ps4-cache').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/auto.html',
+                '/offline.html',
+                '/payloads/goldhen.bin'
+            ]);
+        })
+    );
 });
