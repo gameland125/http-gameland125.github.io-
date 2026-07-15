f8bf110cb8adf09791ff157287b60587aa2bf7d8
gameland125.github.io <farzad03x@gmail.com>
2026-07-15 23:35:12 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -55,3 +55,10 @@ self.addEventListener('fetch', (event) => {
     )
   );
 });
+const CACHE_NAME = 'gameland-v1';
+const ASSETS = ['./', './index.html', './style.css', './main.js', './manifest.webmanifest'];
+
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
