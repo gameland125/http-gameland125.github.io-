0ec230cf35b3d2df4bdf41893a38a9c8bb0d7eab
gameland125.github.io <farzad03x@gmail.com>
2026-07-15 19:36:19 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,4 @@
-'use strict';
-
-const CACHE_NAME = 'gameland-shell-v1';
+const CACHE_NAME = 'gameland-v1';
 const APP_SHELL = [
   './',
   './index.html',
@@ -13,20 +11,20 @@ const APP_SHELL = [
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
   );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(keys.map((key) => (key !== CACHE_NAME ? caches.delete(key) : Promise.resolve())))
+      Promise.all(keys.map((key) => (key === CACHE_NAME ? null : caches.delete(key))))
     ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') return;
   event.respondWith(
     caches.match(event.request).then((cached) => cached || fetch(event.request))
   );
