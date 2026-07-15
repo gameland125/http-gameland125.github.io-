338d9f408a83f00b80b8638db11b3e30f9cf3564
gameland125.github.io <farzad03x@gmail.com>
2026-07-16 01:07:17 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,36 @@
-const CACHE_NAME = 'gameland-v1';
-const ASSETS = ['./', './index.html', './style.css', './main.js', './manifest.webmanifest'];
+const CACHE_NAME = 'gameland-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './main.js',
+  './style.css',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
 
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
